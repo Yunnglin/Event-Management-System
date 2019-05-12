e487a3a5fcb422f72582f1bd4412050145778d00
zjj <632291793@qq.com>
2019-05-12 13:46:43 +0800

    repush

--- src/main/java/com/cms/mapper/ParticipationMapper.java
+++
@@ -1,7 +0,0 @@
-package com.cms.mapper;
-
-import com.cms.pojo.Participation;
-
-public interface ParticipationMapper {
-    int inserP(Participation participationMapper);
-}
--- src/main/java/com/cms/mapper/TeamMapper.java
+++
@@ -1,6 +0,0 @@
-package com.cms.mapper;
-
-public interface TeamMapper {
-    String queryPassword(String s);
-    int queryIdByAccount(String s);
-}
